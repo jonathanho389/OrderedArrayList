48fae67d40cd34b205610efd57b90406ec26ccce
Jonathan <>
2020-12-13 13:24:29 -0500

    coded constructors and add

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,4 +1,17 @@
 import java.util.*;
-public class NoNullArrayList{
-  
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+
+  public boolean add(T element){
+    if(element == null){
+      throw new IllegalArgumentException("You can't add null values");
+    }
+    return super.add(element);
+  }
 }
--- nullTester.java
+++ nullTester.java
@@ -1,4 +1,9 @@
 import java.util.*;
 public class nullTester{
-
+  public static void main(String[] args) {
+    NoNullArrayList<Integer> pain = new NoNullArrayList<Integer>(5);
+    pain.add(1);
+    System.out.println(pain.get(0));
+    pain.add(null);
+  }
 }
